04a7a0f9b9fa761ee6f31c0e2df73ff78adb043a
Niels Overwijn <niels.overwijn@gmail.com>
2013-11-10 23:11:39 +0100

    Using espeak for TTS

--- RobinMouth.js
+++ RobinMouth.js
@@ -2,11 +2,23 @@
 var spawn = require('child_process').spawn;
 var tts = null;
 var printoutput = true;
-var printerrors = false;
-var printexits = false;
+var printerrors = true;
+var printexits = true;
 
+var voice = 'default';
+var voiceVariant = '+2';
+var pitch = 50; // Adjusts the pitch in a range of 0 to 99. The default is 50.
+var amplitude = 100; // Sets amplitude (volume) in a range of 0 to 200. The default is 100.
+var speed = 140; // Sets the speed in words-per-minute.
+var wordGap = 0; // Word gap. This option inserts a pause between words. The value is the length of the pause, in units of 10 mS
 
-tts = spawn('festival');
+tts = spawn('espeak', [
+	'-v', voice+voiceVariant,
+	'-p', pitch,
+	'-a', amplitude,
+	'-s', speed,
+	'-g', wordGap
+]);
 
 tts.stdout.on('data', function (data)
 {
@@ -30,5 +42,6 @@ tts.on('close', function (code)
 
 exports.say = function (message)
 {
-	tts.stdin.write("(SayText \"" + message + "\")");
-}
+	//tts.stdin.write("(SayText \"" + message + "\")");
+	tts.stdin.write(message+"\n");
+};
