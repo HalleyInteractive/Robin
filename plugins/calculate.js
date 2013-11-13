4779b160935970935ada98077404e43f5e29fa59
Niels Overwijn <niels.overwijn@gmail.com>
2013-11-13 22:15:46 +0100

    Fixed rounding

--- plugins/calculate.js
+++ plugins/calculate.js
@@ -1,32 +1,28 @@
 function plus(cmd)
 {
 	console.log("Running the PLUS calculate callback");
-	console.log(cmd);
-	exports.say("The sum of " + cmd[1] + " plus " + cmd[2] + " is " + (parseFloat(cmd[1]) + parseFloat(cmd[2])));
+	exports.say("The sum of " + cmd[1] + " plus " + cmd[2] + " is " + (parseFloat(cmd[1]) + parseFloat(cmd[2])).toFixed(2));
 }
 
 function minus(cmd)
 {
 	console.log("Running the MINUS calculate callback");
-	console.log(cmd);
-	exports.say("The sum of " + cmd[1] + " minus " + cmd[2] + " is " + (parseFloat(cmd[1]) - parseFloat(cmd[2])));
+	exports.say("The sum of " + cmd[1] + " minus " + cmd[2] + " is " + (parseFloat(cmd[1]) - parseFloat(cmd[2])).toFixed(2));
 }
 
 function divided(cmd)
 {
 	console.log("Running the DIVIDED calculate callback");
-	console.log(cmd);
-	exports.say("The sum of " + cmd[1] + " divided " + cmd[2] + " is " + (parseFloat(cmd[1]) / parseFloat(cmd[2])));
+	exports.say("The sum of " + cmd[1] + " divided by" + cmd[2] + " is " + (parseFloat(cmd[1]) / parseFloat(cmd[2])).toFixed(2));
 }
 
 function times(cmd)
 {
 	console.log("Running the TIMES calculate callback");
-	console.log(cmd);
-	exports.say("The sum of " + cmd[1] + " times " + cmd[2] + " is " + (parseFloat(cmd[1]) * parseFloat(cmd[2])));
+	exports.say("The sum of " + cmd[1] + " times " + cmd[2] + " is " + (parseFloat(cmd[1]) * parseFloat(cmd[2])).toFixed(2));
 }
 
-exports.extendedCommand =
+exports.extendedCommands =
 [
 	{command:"WHAT IS THE SUM OF (\\d+) PLUS (\\d+)", callback:plus},
 	{command:"WHAT IS THE SUM OF (\\d+) MINUS (\d+)", callback:minus},
