605b1930b923cf441b3207c31c7f10f7f9b46ad9
Niels Overwijn <niels.overwijn@gmail.com>
2013-11-15 07:16:06 +0100

    Que messages when talking

--- RobinMouth.js
+++ RobinMouth.js
@@ -4,24 +4,50 @@ var speech = require('node-tts');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var tts = null;
+var talking = false;
+var que = [];
 
-exports.say = function (message)
+exports.say = function (message, callback)
 {
-	// TODO: Check if audio is still playing. If it is, que the new request
-	console.log(exports.robin);
-	speech.retrieve(message, exports.robin.language.toString().substr(0,2), function(text, lang, data)
+	if(!talking)
 	{
-		fs.writeFile("input.mp3", data, function(err)
+		talking = true;
+		speech.retrieve(message, exports.robin.language.toString().substr(0,2), function(text, lang, data)
 		{
-			if (err)
+			fs.writeFile("input.mp3", data, function(err)
 			{
-				console.log("Error saving voice input to the disk: " + err);
-			}
-			else {
-				tts = spawn('mplayer', ["input.mp3"]);
-				tts.stderr.on('data', function (data) { console.log('Error playing voice input: ' + data); });
-				tts.on('close', function (code) { console.log('child process exited with code ' + code); });
-			}
-		});
-	}, function(){ console.log("Error retrieving voice data"); });
+				if (err)
+				{
+					console.log("Error saving voice input to the disk: " + err);
+					doneTalking(callback);
+				}
+				else {
+					tts = spawn('mplayer', ["input.mp3"]);
+					tts.on('close', function (code)
+					{
+						console.log('child process exited with code ' + code);
+						doneTalking(callback);
+					});
+				}
+			});
+		}, function(){ console.log("Error retrieving voice data"); doneTalking(callback); });
+	} else
+	{
+		console.log("Que message: " + message);
+		que.push({message:message, callback:callback});
+	}
 };
+
+function doneTalking(callback)
+{
+	console.log("Done talking");
+	talking = false;
+	if(callback !== undefined && callback !== null) { callback(); }
+	if(que.length > 0)
+	{
+		console.log("Que not empty: ");
+		var m = que.shift();
+		console.log(m);
+		exports.say(m.message, m.callback);
+	}
+}
