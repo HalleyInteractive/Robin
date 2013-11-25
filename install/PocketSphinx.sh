7c24d9a1a6fa1257919a02879e4b53fd8facb287
Niels Overwijn <niels.overwijn@gmail.com>
2013-11-26 00:22:27 +0100

    Changed some install lines

--- install/PocketSphinx.sh
+++ install/PocketSphinx.sh
@@ -7,20 +7,27 @@ sudo mv /usr/include/pulse/pulseaudio.h /usr/include/pulse/pulseaudio.h.old
 sudo apt-get install alsa-utils -y
 sudo apt-get install bison -y
 sudo apt-get install libasound2-dev -y
-sudo apt-get install pocketsphinx-utils -y
 
-# wget http://sourceforge.net/projects/cmusphinx/files/sphinxbase/0.8/sphinxbase-0.8.tar.gz
-# gzip -d sphinxbase-0.8.tar.gz
-# tar -xvf sphinxbase-0.8.tar
-# cd sphinxbase-0.8
-# ./configure
-# make
-# sudo make install
+# sudo apt-get install gstreamer0.10-pocketsphinx
 
-# wget http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz
-# gzip -d pocketsphinx-0.8.tar.gz
-# tar -xvf pocketsphinx-0.8.tar
-# cd pocketsphinx-0.8
-# ./configure
-# make
-# sudo make install
+wget http://sourceforge.net/projects/cmusphinx/files/sphinxbase/0.8/sphinxbase-0.8.tar.gz
+gzip -d sphinxbase-0.8.tar.gz
+tar -xvf sphinxbase-0.8.tar
+cd sphinxbase-0.8
+./configure
+make
+sudo make install
+cd ../
+rm sphinxbase-0.8.tar
+rm -rf sphinxbase-0.8
+
+wget http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz
+gzip -d pocketsphinx-0.8.tar.gz
+tar -xvf pocketsphinx-0.8.tar
+cd pocketsphinx-0.8
+./configure
+make
+sudo make install
+cd ../
+rm pocketsphinx-0.8.tar
+rm -rf pocketsphinx-0.8
