266deefe1f06ca775e32bce6b3f6b429dc0d84c8
Niels Overwijn <niels.overwijn@gmail.com>
2013-11-28 07:25:51 +0100

    Request next input implemented for plugins

--- Robin.js
+++ Robin.js
@@ -4,8 +4,8 @@ var mouth = require('./RobinMouth.js');
 var brain = require('./RobinBrain.js');
 var config = require('./RobinConfig.js');
 
-ears.basiccmd = function (cmd) { runBasicCommand(cmd); };
-ears.extendedcmd = function (cmd) { runExtendedCommand(cmd); };
+ears.basiccmd = runBasicCommand;
+ears.extendedcmd = runExtendedCommand;
 ears.robin = config.robin;
 ears.stt_basic_start();
 ears.brain = brain.brain;
@@ -13,14 +13,12 @@ ears.brain = brain.brain;
 mouth.robin = config.robin;
 mouth.brain = brain.brain;
 
-// TODO: Check if it's needed to start redis?
-// TODO: Let plugins receive all next input on request.
 // TODO: Make a backend. include tty.js
 // TODO: Convert written digits to digits
 // TODO: Write documentation
 
 var words = [];
-
+var requestedNextExtendedInput = undefined;
 var registeredBasicCommands = [];
 var registeredExtendedCommands = [];
 var plugins =
@@ -30,7 +28,9 @@ var plugins =
 	repeat: require('./plugins/repeat'),
 	jokes: require('./plugins/jokes'),
 	hello: require('./plugins/hello'),
-	calculate: require('./plugins/calculate')
+	calculate: require('./plugins/calculate'),
+    disappoint: require('./plugins/disappoint'),
+    acknowledge: require('./plugins/acknowledge')
 };
 
 for(var plugin in plugins)
@@ -47,9 +47,19 @@ for(var plugin in plugins)
 		registeredExtendedCommands[language] = registeredExtendedCommands[language].concat(plugins[plugin].extendedCommands[language]);
 	}
 
+    plugins[plugin].plugins = plugins;
 	plugins[plugin].say = mouth.say;
-	plugins[plugin].robin = config.robin;
 	plugins[plugin].ears = ears;
+	plugins[plugin].robin = config.robin;
+    plugins[plugin].requestNextExtendedInput = requestNextExtendedInput;
+}
+
+function requestNextExtendedInput(callback)
+{
+    console.log("Getting ready for next input");
+    requestedNextExtendedInput = callback;
+    ears.stt_basic_stop();
+	ears.stt_extended_start();
 }
 
 function listenForExtendedCommand(cmd)
@@ -87,31 +97,43 @@ stream.once('open', function()
 
 function runBasicCommand(cmd)
 {
+    var foundMatch = false;
 	for(var i = 0; i < registeredBasicCommands[config.robin.language].length; i++)
 	{
 		var match = cmd.match(registeredBasicCommands[config.robin.language][i].command);
 		if(match)
 		{
 			console.log("Found a registered command");
+            foundMatch = true;
 			registeredBasicCommands[config.robin.language][i].callback(match);
 		}
 	}
+    if(!foundMatch){ plugins.disappoint.didNotUnderstand(); }
 }
 
 function runExtendedCommand(cmd)
 {
-	for(var i = 0; i < registeredExtendedCommands[config.robin.language].length; i++)
-	{
-		var match = cmd.match(registeredExtendedCommands[config.robin.language][i].command);
-		if(match)
-		{
-			console.log("Found a extended registered command");
-			registeredExtendedCommands[config.robin.language][i].callback(match);
-			if(registeredExtendedCommands[config.robin.language][i].save !== false)
-			{
-				config.lastCommand = registeredExtendedCommands[config.robin.language][i];
-			}
-		}
-		//TODO: When nothing is found, let down
-	}
+    if(requestedNextExtendedInput === null || requestedNextExtendedInput === undefined)
+    {
+         var foundMatch = false;
+        for(var i = 0; i < registeredExtendedCommands[config.robin.language].length; i++)
+        {
+            var match = cmd.match(registeredExtendedCommands[config.robin.language][i].command);
+            if(match)
+            {
+                console.log("Found a extended registered command");
+                foundMatch = true;
+                registeredExtendedCommands[config.robin.language][i].callback(match);
+                if(registeredExtendedCommands[config.robin.language][i].save !== false)
+                {
+                    config.lastCommand = registeredExtendedCommands[config.robin.language][i];
+                }
+            }
+        }
+        if(!foundMatch){ plugins.disappoint.didNotUnderstand(); }
+	} else
+    {
+        requestedNextExtendedInput(cmd);
+        requestedNextExtendedInput = null;
+    }
 }
