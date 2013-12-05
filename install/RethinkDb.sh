07112258cc701613c014784094cb68db2fdfc4e6
Niels Overwijn <niels.overwijn@gmail.com>
2013-12-05 07:27:04 +0100

    Backup and restore clean db

--- install/RethinkDb.sh
+++ install/RethinkDb.sh
@@ -1,3 +1,15 @@
 sudo add-apt-repository ppa:rethinkdb/ppa   && \
 sudo apt-get update                         && \
 sudo apt-get install rethinkdb
+
+# Starting rethinkdb
+# rethinkdb --http-port 8083
+
+sudo apt-get install python-pip
+sudo pip install rethinkdb
+
+# Backup rethinkdb
+# rethinkdb dump -f install/robin_db_dump.tar.gz -e Robin
+
+# Restore backup
+rethinkdb restore install/robin_db_dump.tar.gz
