4b44a72366fecb412eb2fbd0e3377c5d03e354c5
Niels Overwijn <niels.overwijn@gmail.com>
2013-12-18 20:04:59 +0100

    Files are hosted

--- RobinServer.js
+++ RobinServer.js
@@ -1,5 +1,30 @@
-var io = require('socket.io').listen(8092);
-io.set('log level', 1);
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var mime = require('mime');
+
+//io.set('log level', 1);
+app.listen(8092);
+
+function handler(request, response)
+{
+
+	var file = __dirname + (request.url == '/' ? '/server/index.html' : '/server/' + request.url);
+	console.log("File: " + file);
+	content_type = mime.lookup(file);
+    fs.readFile(file, function(error, data)
+	{
+        if (error)
+		{
+            response.writeHead(500);
+            return response.end('Error loading ' + file);
+        }
+        response.setHeader('Content-Type', content_type);
+		response.writeHead(200);
+		response.end(data);
+    });
+}
+
 io.sockets.on('connection', function (socket)
 {
 	socket.on('basiccmd', basiccmd);
