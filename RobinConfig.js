5a47cf4a5920705ca3f65ffe54439f3468bb8a87
Niels Overwijn <niels.overwijn@gmail.com>
2014-01-04 12:27:00 +0100

    Added version

--- RobinConfig.js
+++ RobinConfig.js
@@ -4,12 +4,14 @@
 * Contains the settings for Robin
 * @param String name Name that Robin should listen to
 * @param String language Language is used to convert text to speech and speech recognition
+* @param String Version of Robin
 * @param Object lastCommand last command is saved here for reuse.
 */
 var Robin =
 {
 	name: "James",
 	language: "en-US",
+	version: '0.0.1',
 	lastCommand:{}
 };
 
