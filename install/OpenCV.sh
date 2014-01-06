3801b7243cc8063c230a0b3ab653b76bd98ad73e
Niels Overwijn <niels.overwijn@gmail.com>
2014-01-06 07:04:35 +0100

    auto install

--- install/OpenCV.sh
+++ install/OpenCV.sh
@@ -1,10 +1,10 @@
 sudo apt-get install cmake -y
 
 sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install libavformat-dev
-sudo apt-get install x264 v4l-utils ffmpeg
-sudo apt-get install libcv2.3 libcvaux2.3 libhighgui2.3 python-opencv opencv-doc libcv-dev libcvaux-dev libhighgui-dev
+sudo apt-get install build-essential -y
+sudo apt-get install libavformat-dev -y
+sudo apt-get install x264 v4l-utils ffmpeg -y
+sudo apt-get install libcv2.3 libcvaux2.3 libhighgui2.3 python-opencv opencv-doc libcv-dev libcvaux-dev libhighgui-dev -y
 
 wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.7/opencv-2.4.7.tar.gz
 tar -xzvf opencv-2.4.7.tar.gz
