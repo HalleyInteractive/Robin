2dd8be84a48fcdabc5f36a74f77502f1d1a1eaf0
Niels Overwijn <niels.overwijn@gmail.com>
2014-01-26 20:49:52 +0100

    Added Dutch

--- plugins/selfie.js
+++ plugins/selfie.js
@@ -1,11 +1,20 @@
-/* globals exports */
+/* globals exports, global */
 
 var countdown = 0;
 
 function selfie()
 {
 	console.log("Running the selfie callback");
-    exports.say("Taking selfie in 3");
+	switch(global.robin.language)
+	{
+	case "en-US":
+	default:
+		exports.say("Taking selfie in 3");
+	break;
+	case "nl-NL":
+		exports.say("Neem een selfie in 3");
+	break;
+	}
     countdown = 3;
     setTimeout(selfieCountdownHandler, 1000);
 }
@@ -23,4 +32,11 @@ exports.extendedCommands['en-US'] =
 	{command:"TAKE PICTURE", callback:selfie},
     {command:"TAKE STILL", callback:selfie},
     {command:"TAKE PHOTO", callback:selfie}
+];
+exports.extendedCommands['nl-NL'] =
+[
+	{command:"NEEM SELFIE", callback:selfie},
+    {command:"NEEM FOTO", callback:selfie},
+    {command:"FOTO NEMEN", callback:selfie},
+    {command:"FOTO MAKEN", callback:selfie}
 ];
