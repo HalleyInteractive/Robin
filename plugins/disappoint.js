f09e4a8234f56243b67a93abd3376c622c2231f3
Niels Overwijn <niels.overwijn@gmail.com>
2014-01-26 20:45:03 +0100

    Added Dutch

--- plugins/disappoint.js
+++ plugins/disappoint.js
@@ -1,10 +1,26 @@
 function didNotUnderstand()
 {
-    var response = [
+	var response = [];
+	switch(global.robin.language)
+	{
+	case "en-US":
+	default:
+		response = [
         "Sorry, I did not get that",
         "I did not understand that",
         "That sounds like chinese"
-    ];
+		];
+	break;
+	case "nl-NL":
+		response = [
+        "Sorry, dat verstond ik niet",
+        "Dat verstond ik niet",
+        "Spreek je chinees ofzo? Ik begrijp niets van wat je zegt"
+		];
+	break;
+	}
+
+
     exports.say(response[Math.floor(Math.random()*response.length)]);
 }
 
