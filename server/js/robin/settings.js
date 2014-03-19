e812a0707bfcfeeb4fffd47eeee4d49aa8bb48b6
Niels Overwijn <niels.overwijn@gmail.com>
2014-03-19 06:55:42 +0100

    Moved settings script to js/robin folder

--- server/js/robin/settings.js
+++ server/js/robin/settings.js
@@ -1,6 +1,28 @@
 /* globals document, io, $ */
 
+var reloadplugins = false;
+
 $(document).ready(function()
 {
+	var editableSettings =
+	{
+		url: 'http://' + window.location.hostname + ':8529/_db/Robin/_api/document/#{settings._id}',
+		pk: '#{settings._key}',
+		ajaxOptions: { type: 'put', dataType: 'json' },
+		params: getSettings,
+		success: function() { socket.emit('reload_settings', ''); if(reloadPlugins){ reloadPlugins = false; socket.emit('reload_plugins', ''); } }
+	}
+	$('#name').editable(editableSettings);
+	$('#audiodevice').editable(editableSettings);
 
+	editableSettings.source = languages.split(",");
+	$('#language').editable(editableSettings);
 });
+
+function getSettings(changedparam)
+{
+	var settings = defaultSettings;
+	settings[changedparam.name] = changedparam.value;
+	reloadPlugins = changedparam.name === 'name';
+	return JSON.stringify(settings);
+}
