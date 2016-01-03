bf644611e70080b7bdabceb7ee8ffc8682a7bb6c
Niels Overwijn <niels.overwijn@gmail.com>
2016-01-03 22:55:35 +0000

    Refactored legs

--- Legs.py
+++ Legs.py
@@ -4,63 +4,55 @@ from Nerves import Nerves
 
 class Legs(Nerves):
 
-	motor_left_forward = 23
-	motor_left_backward = 24
-	motor_left_enable = 25
+	motor_left = {
+		'forward' : 23,
+		'backward' : 24,
+		'enable' : 25
+	}
 
-	motor_right_forward = 17
-	motor_right_backward = 27
-	motor_right_enable = 22
+	motor_right = {
+		'forward' : 17,
+		'backward' : 27,
+		'enable' : 22
+	}
+
+	DIRECTION_FORWARD = 'forward'
+	DIRECTION_BACKWARD = 'backward'
 
 	def __init__(self):
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.motor_left_forward, GPIO.OUT)
-		GPIO.setup(self.motor_left_backward, GPIO.OUT)
-		GPIO.setup(self.motor_left_enable, GPIO.OUT)
-		GPIO.setup(self.motor_right_forward, GPIO.OUT)
-		GPIO.setup(self.motor_right_backward, GPIO.OUT)
-		GPIO.setup(self.motor_right_enable, GPIO.OUT)
+		GPIO.setup(self.motor_left.forward, GPIO.OUT)
+		GPIO.setup(self.motor_left.backward, GPIO.OUT)
+		GPIO.setup(self.motor_left.enable, GPIO.OUT)
+		GPIO.setup(self.motor_right.forward, GPIO.OUT)
+		GPIO.setup(self.motor_right.backward, GPIO.OUT)
+		GPIO.setup(self.motor_right.enable, GPIO.OUT)
 
 	def forward(self):
-		GPIO.output(self.motor_left_forward, GPIO.HIGH)
-		GPIO.output(self.motor_left_backward, GPIO.LOW)
-		GPIO.output(self.motor_right_forward, GPIO.HIGH)
-		GPIO.output(self.motor_right_backward, GPIO.LOW)
-
-		GPIO.output(self.motor_left_enable, GPIO.HIGH)
-		GPIO.output(self.motor_right_enable, GPIO.HIGH)
+		set_motor(self.motor_left, self.DIRECTION_FORWARD)
+		set_motor(self.motor_right, self.DIRECTION_FORWARD)
 
 	def backward(self):
-		GPIO.output(self.motor_left_forward, GPIO.LOW)
-		GPIO.output(self.motor_left_backward, GPIO.HIGH)
-		GPIO.output(self.motor_right_forward, GPIO.LOW)
-		GPIO.output(self.motor_right_backward, GPIO.HIGH)
-
-		GPIO.output(self.motor_left_enable, GPIO.HIGH)
-		GPIO.output(self.motor_right_enable, GPIO.HIGH)
+		set_motor(self.motor_left, self.DIRECTION_BACKWARD)
+		set_motor(self.motor_right, self.DIRECTION_BACKWARD)
 
 	def left(self):
-		GPIO.output(self.motor_left_forward, GPIO.HIGH)
-		GPIO.output(self.motor_left_backward, GPIO.LOW)
-		GPIO.output(self.motor_right_forward, GPIO.LOW)
-		GPIO.output(self.motor_right_backward, GPIO.HIGH)
-
-		GPIO.output(self.motor_left_enable, GPIO.HIGH)
-		GPIO.output(self.motor_right_enable, GPIO.HIGH)
+		set_motor(self.motor_left, self.DIRECTION_BACKWARD)
+		set_motor(self.motor_right, self.DIRECTION_FORWARD)
 
 	def right(self):
-		GPIO.output(self.motor_left_forward, GPIO.LOW)
-		GPIO.output(self.motor_left_backward, GPIO.HIGH)
-		GPIO.output(self.motor_right_forward, GPIO.HIGH)
-		GPIO.output(self.motor_right_backward, GPIO.LOW)
-
-		GPIO.output(self.motor_left_enable, GPIO.HIGH)
-		GPIO.output(self.motor_right_enable, GPIO.HIGH)
+		set_motor(self.motor_left, self.DIRECTION_FORWARD)
+		set_motor(self.motor_right, self.DIRECTION_BACKWARD)
 
 	def stop(self):
-		GPIO.output(self.motor_left_enable, GPIO.HIGH)
-		GPIO.output(self.motor_right_enable, GPIO.LOW)
+		GPIO.output(self.motor_left.enable, GPIO.HIGH)
+		GPIO.output(self.motor_right.enable, GPIO.LOW)
 		GPIO.cleanup()
+
+	def set_motor(motor, direction):
+		GPIO.output(motor.forward, GPIO.HIGH if direction is self.DIRECTION_FORWARD else GPIO.LOW)
+		GPIO.output(motor.backward, GPIO.HIGH if direction is self.DIRECTION_BACKWARD else GPIO.LOW)
+		GPIO.output(motor.enable, GPIO.HIGH)
 
 legs = Legs()
 legs.forward()
